f82f82f2bd2c7594ffe9c5cc4e78e58697db9b83
Daniil Gasnikov <legionlon@mail.ru>
2021-02-16 19:57:18 +0500

    added shit

---
+++ app.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import telebot
+import time
+from datetime import date, datetime
+from telebot import types
+import requests
+import random
+import re
+from pymystem3 import Mystem
+
+token = 'enter_your_token'
+
+bot = telebot.TeleBot(token)
+m = Mystem()
+
+
+@bot.message_handler(commands=["rp"])
+def rare_parrot(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'http://i.imgur.com/2czseQm.gif')
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    try:
+        words = m.lemmatize(message.text)
+        words = [x.strip() for x in words if len(x.strip()) > 1]
+        chosen_one = random.choice(words).strip()
+        if random.random() > 0.5 and len(chosen_one) > 0 and chosen_one != " ":
+            print(' '.join(words), f"'{chosen_one}'")
+            bot.reply_to(message, f"{chosen_one} у тебя в штанах")
+    except:
+        pass
+
+   
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True, timeout=123)
